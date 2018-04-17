a33693f8c2c32747ecc8937548ee9a90ba5bfe0b
slee4354 <slee4354@conestogac.on.ca>
2018-04-17 13:16:26 -0400

    index url page

--- mysite/views.py
+++ mysite/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
 def indexPage(request):
-    return render(request, 'blog/index.html', {})
+    return render(request, 'mysite/templates/blog/index.html', {})
 # Create your views here.
